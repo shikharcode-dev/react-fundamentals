86c1936594b80f52e6a9acc4e95fe236988f6f1d
Shikhar Mishra <mshikhar353@gmail.com>
2026-07-01 15:29:53 +0530

    explain hooks and rerander

--- 3rd_day/src/App.jsx
+++ 3rd_day/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import React from 'react'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
@@ -5,12 +6,11 @@ import Hero from './components/Hero'
 
 const App = () => {
 
-
+  let [count, setCount] = useState(0)
   return (
-    <div style={{backgroundColor: "red"}}>
-      <Navbar/>
-      <Hero/>
-      <Footer/>
+    <div>
+      <h1>Count is - {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Click</button>
     </div>
   )
 }
